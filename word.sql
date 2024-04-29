61a905e95e26dd517b2ce7081e00d08b1be561de
nastyasha18 <131899204+nastyasha18@users.noreply.github.com>
2024-04-29 21:17:52 +0500

    обновление бд

--- word.sql
+++ word.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Апр 29 2024 г., 13:19
+-- Время создания: Апр 29 2024 г., 19:13
 -- Версия сервера: 8.0.30
 -- Версия PHP: 7.4.30
 
@@ -30,7 +30,8 @@ SET time_zone = "+00:00";
 CREATE TABLE `boss_practice_company` (
   `fio` varchar(255) DEFAULT NULL,
   `post` varchar(255) DEFAULT NULL,
-  `name_practice` varchar(255) DEFAULT NULL
+  `name_practice` varchar(255) DEFAULT NULL,
+  `id` bigint UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -42,7 +43,8 @@ CREATE TABLE `boss_practice_company` (
 CREATE TABLE `boss_practice_org_company` (
   `fio` varchar(255) DEFAULT NULL,
   `post` varchar(255) DEFAULT NULL,
-  `name_practice` varchar(255) DEFAULT NULL
+  `name_practice` varchar(255) DEFAULT NULL,
+  `id` bigint UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -54,7 +56,8 @@ CREATE TABLE `boss_practice_org_company` (
 CREATE TABLE `boss_practice_ugrasu` (
   `fio` varchar(255) DEFAULT NULL,
   `post` varchar(255) DEFAULT NULL,
-  `name_practice` varchar(255) DEFAULT NULL
+  `name_practice` varchar(255) DEFAULT NULL,
+  `id` bigint UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -65,7 +68,8 @@ CREATE TABLE `boss_practice_ugrasu` (
 
 CREATE TABLE `comments` (
   `student_fio` varchar(255) DEFAULT NULL,
-  `text_` text
+  `text` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `id` bigint UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -76,7 +80,8 @@ CREATE TABLE `comments` (
 
 CREATE TABLE `direction` (
   `name` varchar(255) DEFAULT NULL,
-  `groupe` varchar(255) DEFAULT NULL
+  `groupe` varchar(255) DEFAULT NULL,
+  `id` bigint UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -88,7 +93,8 @@ CREATE TABLE `direction` (
 CREATE TABLE `groupe` (
   `number` varchar(255) DEFAULT NULL,
   `class` int DEFAULT NULL,
-  `student` varchar(255) DEFAULT NULL
+  `student` varchar(255) DEFAULT NULL,
+  `id` bigint UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -99,7 +105,8 @@ CREATE TABLE `groupe` (
 
 CREATE TABLE `hards` (
   `student_fio` varchar(255) DEFAULT NULL,
-  `text_` text
+  `text` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `id` bigint UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -110,7 +117,8 @@ CREATE TABLE `hards` (
 
 CREATE TABLE `institute` (
   `name` varchar(255) DEFAULT NULL,
-  `direction` varchar(255) DEFAULT NULL
+  `direction` varchar(255) DEFAULT NULL,
+  `id` bigint UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -123,7 +131,8 @@ CREATE TABLE `opop` (
   `fio` varchar(255) DEFAULT NULL,
   `post` varchar(255) DEFAULT NULL,
   `direction` varchar(255) DEFAULT NULL,
-  `user_id` int DEFAULT NULL
+  `user_id` int DEFAULT NULL,
+  `id` bigint UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -134,7 +143,8 @@ CREATE TABLE `opop` (
 
 CREATE TABLE `pay` (
   `student_fio` varchar(255) DEFAULT NULL,
-  `viewe` varchar(255) DEFAULT NULL
+  `viewe` varchar(255) DEFAULT NULL,
+  `id` bigint UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -146,7 +156,8 @@ CREATE TABLE `pay` (
 CREATE TABLE `place_practice` (
   `address` varchar(255) DEFAULT NULL,
   `name_place` varchar(255) DEFAULT NULL,
-  `name_practice` varchar(255) DEFAULT NULL
+  `name_practice` varchar(255) DEFAULT NULL,
+  `id` bigint UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -160,7 +171,8 @@ CREATE TABLE `practice` (
   `srok` varchar(255) DEFAULT NULL,
   `name_practice` varchar(255) DEFAULT NULL,
   `namber_date_order` varchar(255) DEFAULT NULL,
-  `groupe` varchar(255) DEFAULT NULL
+  `groupe` varchar(255) DEFAULT NULL,
+  `id` bigint UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -170,7 +182,7 @@ CREATE TABLE `practice` (
 --
 
 CREATE TABLE `practice_student` (
-  `id` int DEFAULT NULL,
+  `id` int NOT NULL,
   `student_fio` varchar(255) DEFAULT NULL,
   `viewe_contract` varchar(255) DEFAULT NULL,
   `pay` varchar(255) DEFAULT NULL,
@@ -190,7 +202,8 @@ CREATE TABLE `practice_student` (
 
 CREATE TABLE `quality` (
   `student_fio` varchar(255) DEFAULT NULL,
-  `text_` text
+  `text` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `id` bigint UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -201,7 +214,8 @@ CREATE TABLE `quality` (
 
 CREATE TABLE `size_work` (
   `student_fio` varchar(255) DEFAULT NULL,
-  `text_` text
+  `text` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `id` bigint UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -211,7 +225,8 @@ CREATE TABLE `size_work` (
 --
 
 CREATE TABLE `student` (
-  `fio` varchar(255) DEFAULT NULL
+  `fio` varchar(255) DEFAULT NULL,
+  `id` bigint UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -223,7 +238,8 @@ CREATE TABLE `student` (
 CREATE TABLE `tasks` (
   `student_fio` varchar(255) DEFAULT NULL,
   `name_task` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `datee` varchar(255) DEFAULT NULL
+  `datee` varchar(255) DEFAULT NULL,
+  `id` bigint UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -234,7 +250,8 @@ CREATE TABLE `tasks` (
 
 CREATE TABLE `type_practice` (
   `type` varchar(255) DEFAULT NULL,
-  `name_practice` varchar(255) DEFAULT NULL
+  `name_practice` varchar(255) DEFAULT NULL,
+  `id` bigint UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -244,7 +261,7 @@ CREATE TABLE `type_practice` (
 --
 
 CREATE TABLE `users` (
-  `id` int DEFAULT NULL,
+  `id` int NOT NULL,
   `role` varchar(255) DEFAULT NULL,
   `login` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL
@@ -258,7 +275,8 @@ CREATE TABLE `users` (
 
 CREATE TABLE `view_contract` (
   `student_fio` varchar(255) DEFAULT NULL,
-  `viewe` varchar(255) DEFAULT NULL
+  `viewe` varchar(255) DEFAULT NULL,
+  `id` bigint UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -269,8 +287,276 @@ CREATE TABLE `view_contract` (
 
 CREATE TABLE `view_practice` (
   `name_practice` varchar(255) DEFAULT NULL,
-  `viewe` varchar(255) DEFAULT NULL
+  `viewe` varchar(255) DEFAULT NULL,
+  `id` bigint UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `boss_practice_company`
+--
+ALTER TABLE `boss_practice_company`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Индексы таблицы `boss_practice_org_company`
+--
+ALTER TABLE `boss_practice_org_company`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Индексы таблицы `boss_practice_ugrasu`
+--
+ALTER TABLE `boss_practice_ugrasu`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Индексы таблицы `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Индексы таблицы `direction`
+--
+ALTER TABLE `direction`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Индексы таблицы `groupe`
+--
+ALTER TABLE `groupe`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Индексы таблицы `hards`
+--
+ALTER TABLE `hards`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Индексы таблицы `institute`
+--
+ALTER TABLE `institute`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Индексы таблицы `opop`
+--
+ALTER TABLE `opop`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Индексы таблицы `pay`
+--
+ALTER TABLE `pay`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Индексы таблицы `place_practice`
+--
+ALTER TABLE `place_practice`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Индексы таблицы `practice`
+--
+ALTER TABLE `practice`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Индексы таблицы `practice_student`
+--
+ALTER TABLE `practice_student`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `quality`
+--
+ALTER TABLE `quality`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Индексы таблицы `size_work`
+--
+ALTER TABLE `size_work`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Индексы таблицы `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Индексы таблицы `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Индексы таблицы `type_practice`
+--
+ALTER TABLE `type_practice`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `view_contract`
+--
+ALTER TABLE `view_contract`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Индексы таблицы `view_practice`
+--
+ALTER TABLE `view_practice`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `boss_practice_company`
+--
+ALTER TABLE `boss_practice_company`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `boss_practice_org_company`
+--
+ALTER TABLE `boss_practice_org_company`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `boss_practice_ugrasu`
+--
+ALTER TABLE `boss_practice_ugrasu`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `direction`
+--
+ALTER TABLE `direction`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `groupe`
+--
+ALTER TABLE `groupe`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `hards`
+--
+ALTER TABLE `hards`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `institute`
+--
+ALTER TABLE `institute`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `opop`
+--
+ALTER TABLE `opop`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `pay`
+--
+ALTER TABLE `pay`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `place_practice`
+--
+ALTER TABLE `place_practice`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `practice`
+--
+ALTER TABLE `practice`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `quality`
+--
+ALTER TABLE `quality`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `size_work`
+--
+ALTER TABLE `size_work`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `student`
+--
+ALTER TABLE `student`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `type_practice`
+--
+ALTER TABLE `type_practice`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `view_contract`
+--
+ALTER TABLE `view_contract`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `view_practice`
+--
+ALTER TABLE `view_practice`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
